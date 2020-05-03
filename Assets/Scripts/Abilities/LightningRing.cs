bab64b4ebc02226eb498baf313e025167a0907e3
craus90 <craus902@gmail.com>
2020-05-04 02:49:12 +0400

    lightning ring

---
+++ Assets/Scripts/Abilities/LightningRing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LightningRing : AbilityEffect
+{
+    public override bool Available => base.Available && counter.Value > 0;
+
+    public Counter counter;
+
+    public override void Use(Creature user, Creature target) {
+        int power = counter.Value;
+        counter.Reset();
+        target.Hit(power);
+    }
+
+    public override string Text() {
+        return "<color=#ff8000ff>{0}</color>".i(counter.Value);
+    }
+}
--- Assets/Scripts/CreatureText.cs
+++ Assets/Scripts/CreatureText.cs
@@ -9,6 +9,15 @@ public class CreatureText : MonoBehaviour
     public TMPro.TextMeshProUGUI text;
     public string format = "{0}/{1}";
 
+    public static string StatusString(Creature creature) {
+        return "{regeneration}{protection}{armor}{stunned}".i(new Dictionary<string, object>() {
+            { "protection", ProtectionString(creature.protectionUntilEndOfCombat) },
+            { "stunned", StunString(creature.stunned) },
+            { "armor", creature.armor > 0 ? "A{0}".i(creature.armor) : creature.armor < 0 ? "V{0}".i(-creature.armor) : "" },
+            { "regeneration", RegenerationString(creature.regeneration) },
+        });
+    }
+
     public static string StunString(int s, string c = "*") {
         return s < 0 ? StunString(-s, "+") : s < 4 ? c.repeat(s) : c + "{0}".i(s);
     }
@@ -21,8 +30,8 @@ public class CreatureText : MonoBehaviour
         return v > 0 ? "<color=#808080ff>p{0}</color>".i(v) : "";
     }
 
-    public void Update() {
-        text.text = format.i(new Dictionary<string, object>() {
+    public static string FormatCreature(string format, Creature creature) {
+        return format.i(new Dictionary<string, object>() {
             { "damage", creature.damage },
             { "hp", creature.hp },
             { "maxHp", creature.maxHp },
@@ -31,6 +40,11 @@ public class CreatureText : MonoBehaviour
             { "stunned", StunString(creature.stunned) },
             { "armor", creature.armor > 0 ? "A{0}".i(creature.armor) : creature.armor < 0 ? "V{0}".i(-creature.armor) : "" },
             { "regeneration", RegenerationString(creature.regeneration) },
+            { "status", StatusString(creature) },
         });
+    }
+
+    public void Update() {
+        text.text = FormatCreature(format, creature);
     }
 }
