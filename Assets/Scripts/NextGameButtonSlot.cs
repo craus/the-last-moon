004804a0f092c3fb8269fac3f80d330c24fed494
craus90 <craus902@gmail.com>
2020-08-31 21:58:55 +0400

    go next button if you die

---
+++ Assets/Scripts/NextGameButtonSlot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Linq;
+
+public class NextGameButtonSlot : MonoBehaviour
+{
+    public Button button;
+
+    public void Update() {
+        button.gameObject.SetActive(!Player.instance.Alive);
+    }
+}
