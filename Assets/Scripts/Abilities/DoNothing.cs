22e5d86405382c88d5cb2b8fd3c9aae6c7ddd8b5
craus90 <craus902@gmail.com>
2020-09-13 22:53:51 +0400

    do nothing effect (non-target)

---
+++ Assets/Scripts/Abilities/DoNothing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNothing : NonTargetEffect
+{
+    public override void Use(Creature user) {
+    }
+
+    public override string Text(Creature user) {
+        return "-";
+    }
+
+    public override string Description(Creature user) {
+        return $"Does nothing";
+    }
+}
