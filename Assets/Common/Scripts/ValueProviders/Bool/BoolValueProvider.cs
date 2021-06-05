d81e780c78380b441045c1ba98d46b61c8ce7579
craus90 <craus902@gmail.com>
2021-06-05 05:30:54 +0400

    dead music

--- Assets/Common/Scripts/ValueProviders/Bool/BoolValueProvider.cs
+++ Assets/Common/Scripts/ValueProviders/Bool/BoolValueProvider.cs
@@ -5,6 +5,13 @@ using System.Linq;
 
 public abstract class BoolValueProvider : ValueProvider<bool>
 {
+    [Space]
+    [SerializeField] private bool reversed;
+
+    protected virtual bool BoolValue => false;
+
+    public override bool Value => BoolValue != reversed;
+
     [ContextMenu("Preview value")]
     public void PreviewValue()
     {
