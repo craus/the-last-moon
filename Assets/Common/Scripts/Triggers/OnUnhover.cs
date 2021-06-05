7a35f1c3a08a9ad56659eda791490590c508876a
craus90 <craus902@gmail.com>
2021-06-05 07:24:15 +0400

    battle button hover music

---
+++ Assets/Common/Scripts/Triggers/OnUnhover.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Common
+{
+    public class OnUnhover : Trigger, IPointerExitHandler
+    {
+        public void OnPointerExit(PointerEventData eventData) {
+            Run();
+        }
+    }
+}
