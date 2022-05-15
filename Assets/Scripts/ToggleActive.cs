a38e83a945da78b0f8adb9e22d2a517f4958e499
craus90 <craus902@gmail.com>
2022-05-15 15:25:45 +0400

    log is now toggable and scrollable

---
+++ Assets/Scripts/ToggleActive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Linq;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ToggleActive : MonoBehaviour
+{
+    public GameObject target;
+
+    public void Toggle() {
+        target.SetActive(!target.activeSelf);
+    }
+}
